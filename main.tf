a5691c6c0f15a250f29b8dd2a54236ced85059df
Ben Dovaston <ben.dovaston@gmail.com>
2022-02-22 15:58:26 +0000

    Add name tag

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   tags = {
     Function = "Web"
+    Name     = "dovastbe-web"
   }
   user_data = <<-EOF
               #!/bin/bash
