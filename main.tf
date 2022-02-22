6c098421822a070b79beba7f832af21a2940220b
Ben Dovaston <ben.dovaston@gmail.com>
2022-02-22 16:53:34 +0000

    new appcode

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
   tags = {
     Function = "Web"
     Name     = "dovastbe-web"
-    AppCode  = "BEN"
+    AppCode  = "QWE"
   }
   user_data = <<-EOF
               #!/bin/bash
